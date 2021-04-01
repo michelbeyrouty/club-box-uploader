a34424c731de86519ba2088524e4a1e700a29720
michel.beyrouty@washmen.com <Michel El Beyrouty>
2021-04-01 19:44:25 +0300

    Add upload multiple files function 🗂

--- index.js
+++ index.js
@@ -1,59 +1,28 @@
 const express = require('express');
-const busboy = require('connect-busboy');
-const path = require('path');
-const fs = require('fs-extra');
-const CUSTOM = require('./custom');
+
+// Initialize mutler
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './files');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 const app = express();
 
-// Insert middle-ware
-app.use(busboy({
-  highWaterMark: 2 * 1024 * 1024, // 2MiB buffer
-}));
 
-const uploadPath = path.join(__dirname, CUSTOM.FILE_LOCATION); // Register the upload path
-fs.ensureDir(uploadPath); // Make sure that the upload path exits
-
-// Handlers
-
-// Upload handler
-app.route('/upload').post((req, res, next) => {
-
-  req.pipe(req.busboy); // Pipe it trough busboy
-
-  req.busboy.on('file', (fieldname, file, filename) => {
-    console.log(`Upload of '${filename}' started`);
-
-    // Create a write stream of the new file
-    const fstream = fs.createWriteStream(path.join(uploadPath, filename));
-
-    // Pipe it trough
-    file.pipe(fstream);
-
-    // On finish of the upload
-    fstream.on('close', () => {
-      console.log(`Upload of '${filename}' finished`);
-      res.redirect('back');
-    });
-  });
-});
-
-
-// Download handler
-app.route('/download').post((req, res, next) => {
-  const request = require('request');
-
-  const options = {
-    url:     CUSTOM.DROPBOX.DOWNLOAD_URL,
-    method:  'POST',
-    headers: {
-      'Authorization':   `Bearer ${CUSTOM.DROPBOX.TOKEN}`,
-      'Dropbox-API-Arg': '{"path": "/test.png"}',
-    },
-  };
-
-  request(options).pipe(fs.createWriteStream('files/test.png'));
-  return false;
+// Upload multiple files
+app.post('/bulk', upload.array('profiles', 4), (req, res) =>{
+  try {
+    res.send(req.files);
+  } catch (error) {
+    console.log(error);
+    res.send(400);
+  }
 });
 
 
@@ -64,14 +33,18 @@ app.route('/').get((req, res) => {
   res.write('<div id="logo">Club-Box-uploader</div>');
   res.write('</div>');
   res.write('<br></br>');
-  res.write('<form action="upload" method="post" enctype="multipart/form-data">');
+  res.write('<form action="single" method="post" enctype="multipart/form-data">');
   res.write('<input type="file" name="fileToUpload"><br>');
   res.write('<input type="submit">');
   res.write('</form>');
-  res.write('<br></br>');
-  res.write('<form action="download" method="post" enctype="multipart/form-data">');
-  res.write('<input type="submit"> Download dropbox files </input>');
-  res.write('</form>');
+  // res.write('<br></br>');
+  // res.write('<form action="download" method="post" enctype="multipart/form-data">');
+  // res.write('<input type="submit"> Download dropbox files </input>');
+  // res.write('</form>');
+  // res.write('<br></br>');
+  // res.write('<form action="upload2" method="post" enctype="multipart/form-data">');
+  // res.write('<input type="submit"> upload 2 </input>');
+  // res.write('</form>');
   return res.end();
 });
 
@@ -94,3 +67,52 @@ const server = app.listen(3000, () => {
 //         console.error(error);
 //       });
 
+
+// Insert middle-ware
+// app.use(busboy({
+//   highWaterMark: 2 * 1024 * 1024, // 2MiB buffer
+// }));
+
+
+// Upload handler
+// app.route('/upload').post((req, res, next) => {
+
+//   req.pipe(req.busboy); // Pipe it trough busboy
+
+//   req.busboy.on('file', (fieldname, file, filename) => {
+//     console.log(`Upload of '${filename}' started`);
+
+//     // Create a write stream of the new file
+//     const fstream = fs.createWriteStream(path.join(uploadPath, filename));
+
+//     // Pipe it trough
+//     file.pipe(fstream);
+
+//     // On finish of the upload
+//     fstream.on('close', () => {
+//       console.log(`Upload of '${filename}' finished`);
+//       res.redirect('back');
+//     });
+//   });
+// });
+
+
+// const uploadPath = path.join(__dirname, CUSTOM.FILE_LOCATION); // Register the upload path
+// fs.ensureDir(uploadPath); // Make sure that the upload path exits
+
+
+// app.route('/download').post((req, res, next) => {
+//   const request = require('request');
+
+//   const options = {
+//     url:     CUSTOM.DROPBOX.DOWNLOAD_URL,
+//     method:  'POST',
+//     headers: {
+//       'Authorization':   `Bearer ${CUSTOM.DROPBOX.TOKEN}`,
+//       'Dropbox-API-Arg': '{"path": "/test.png"}',
+//     },
+//   };
+
+//   request(options).pipe(fs.createWriteStream('files/test.png'));
+//   return false;
+// });
